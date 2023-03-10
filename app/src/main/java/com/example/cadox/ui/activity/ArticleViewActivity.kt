9027e22619d87c73cec77f26320d5799e7307977
riderofzion <quentinrouet@hotmail.fr>
2023-03-10 09:13:31 +0100

    Correction TP Module 4 (databidning)

--- app/src/main/java/com/example/cadox/ui/activity/ArticleViewActivity.kt
+++ app/src/main/java/com/example/cadox/ui/activity/ArticleViewActivity.kt
@@ -3,43 +3,30 @@ package com.example.cadox.ui.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
+import androidx.databinding.DataBindingUtil
 import com.example.cadox.R
+import com.example.cadox.databinding.ActivityArticleViewBinding
 import com.example.cadox.repository.ArticleRepository
 import com.google.android.material.snackbar.Snackbar
 
 class ArticleViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_article_view)
-        val article = ArticleRepository.getArticle(1)
-
+        val aavb : ActivityArticleViewBinding= DataBindingUtil.setContentView(
+            this,
+            R.layout.activity_article_view)
+        aavb.article= ArticleRepository.getArticle(1)
         //Afficher les éléments de l'article 1 dans les différentes vues
         //de activity_article_view
-        val textViewIntitule = findViewById<TextView>(R.id.textViewTitle)
-        val textViewDateBuy = findViewById<TextView>(R.id.textViewDateBuy)
-        val textViewPrice = findViewById<TextView>(R.id.textViewPrice)
-        val textViewDescription = findViewById<TextView>(R.id.textViewDescription)
-        val textViewUrl = findViewById<TextView>(R.id.textViewUrl)
-        val ratingBarLevel = findViewById<RatingBar>(R.id.ratingBarLevel)
-        val checkBoxIsBought = findViewById<CheckBox>(R.id.checkBoxIsBought)
-        val buttonEdit = findViewById<ImageButton>(R.id.buttonEdit)
-        val buttonInternet = findViewById<ImageButton>(R.id.buttonInternet)
-        if(article == null) Snackbar.make(
-            textViewIntitule,
+        if(aavb.article == null) Snackbar.make(
+            aavb.root,
             "Article Indisponible",
             12).show()
         else{
-            textViewIntitule.text = article.intitule
-            textViewDateBuy.text = article.dateAchat.toString()
-            textViewPrice.text = article.prixEuro()
-            textViewDescription.text = article.description
-            textViewUrl.text = article.url
-            ratingBarLevel.rating = article.niveau.toFloat()
-            checkBoxIsBought.isChecked = article.achete
-            buttonInternet.setOnClickListener {
+            aavb.buttonInternet.setOnClickListener {
                 Snackbar.make(
-                    buttonInternet,
-                    article.url ?: "pas d'url",
+                    aavb.root,
+                    aavb.article?.url ?: "pas d'url",
                     Snackbar.LENGTH_LONG).show()
             }
         }
