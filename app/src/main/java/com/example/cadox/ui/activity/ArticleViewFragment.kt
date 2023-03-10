86478632787a70266edf99657dfe872a56bbcb58
riderofzion <quentinrouet@hotmail.fr>
2023-03-10 14:03:19 +0100

    Debut TP nAvigation

---
+++ app/src/main/java/com/example/cadox/ui/activity/ArticleViewFragment.kt
@@ -0,0 +1,46 @@
+package com.example.cadox.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.cadox.R
+import com.example.cadox.databinding.FragmentArticleViewBinding
+import com.example.cadox.repository.ArticleRepository
+import com.google.android.material.snackbar.Snackbar
+
+class ArticleViewFragment : Fragment() {
+    lateinit var favb : FragmentArticleViewBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        favb = FragmentArticleViewBinding.inflate(
+            inflater,container,false
+        )
+        return favb.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        favb.article= ArticleRepository.getArticle(1)
+        //Afficher les éléments de l'article 1 dans les différentes vues
+        //de activity_article_view
+        if(favb.article == null) Snackbar.make(
+            favb.root,
+            "Article Indisponible",
+            12).show()
+        else{
+            favb.buttonInternet.setOnClickListener {
+                Snackbar.make(
+                    favb.root,
+                    favb.article?.url ?: "pas d'url",
+                    Snackbar.LENGTH_LONG).show()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/cadox/ui/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.cadox.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cadox.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
